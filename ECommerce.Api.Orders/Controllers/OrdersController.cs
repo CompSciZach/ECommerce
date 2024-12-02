8fae0726251f15b01df38a3a26d1606f7522a691
CompSciZach <2273406@johnabbottcollege.net>
2024-12-02 15:42:00 -0500

    Completed Order: Controller, updated startup and order provider

--- ECommerce.Api.Orders/Controllers/OrdersController.cs
+++ ECommerce.Api.Orders/Controllers/OrdersController.cs
@@ -1,6 +1,29 @@
-﻿namespace ECommerce.Api.Orders.Controllers
+﻿using ECommerce.Api.Orders.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Orders.Controllers
 {
-    public class OrdersController
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
     {
+        private readonly IOrdersProvider ordersProvider;
+
+        public OrdersController(IOrdersProvider ordersProvider)
+        {
+            this.ordersProvider = ordersProvider;
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetOrdersAsync(int customerId)
+        {
+            var result = await ordersProvider.GetOrdersAsync(customerId);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Orders);
+            }
+            return NotFound();
+        }
     }
 }
