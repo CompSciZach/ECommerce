6640371137b05e75a5a229f2fe9096768acfee1a
CompSciZach <2273406@johnabbottcollege.net>
2024-12-02 16:03:52 -0500

    Customers: Implemented DB files, updated startup and added provider

--- ECommerce.Api.Customers/Db/CustomersDbContext.cs
+++ ECommerce.Api.Customers/Db/CustomersDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ECommerce.Api.Customers.Db
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Customers.Db
 {
-    public class CustomersDbContext
+    public class CustomersDbContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+
+        public CustomersDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
