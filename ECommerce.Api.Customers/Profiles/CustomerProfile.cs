b8f15ae4932e1d1e11b373aab0f045f2b6924c06
CompSciZach <2273406@johnabbottcollege.net>
2024-12-02 15:59:59 -0500

    Implemented Customer: Interface, Model, Profile classes

--- ECommerce.Api.Customers/Profiles/CustomerProfile.cs
+++ ECommerce.Api.Customers/Profiles/CustomerProfile.cs
@@ -1,6 +1,10 @@
 ﻿namespace ECommerce.Api.Customers.Profiles
 {
-    public class CustomerProfile
+    public class CustomerProfile : AutoMapper.Profile
     {
+        public CustomerProfile()
+        {
+            CreateMap<Db.Customer, Models.Customer>();
+        }
     }
 }
