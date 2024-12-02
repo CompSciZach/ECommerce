9fc84c930097a2598dff1adb8e7dc38f2c6b2ebe
CompSciZach <2273406@johnabbottcollege.net>
2024-12-02 15:40:22 -0500

    Implemented order: Interface, Model files and profile classes

--- ECommerce.Api.Orders/Interfaces/IOrdersProvider.cs
+++ ECommerce.Api.Orders/Interfaces/IOrdersProvider.cs
@@ -1,6 +1,11 @@
-﻿namespace ECommerce.Api.Orders.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Orders.Interfaces
 {
-    public class IOrdersProvider
+    public interface IOrdersProvider
     {
+        // use cutomser ID to get their orders
+        Task<(bool IsSuccess, IEnumerable<Models.Order> Orders, string ErrorMessage)> GetOrdersAsync(int customerId);
     }
 }
