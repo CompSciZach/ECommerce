57ed1ad5f4fd4167a912d4bf0186f9fd7c7f87c6
CompSciZach <2273406@johnabbottcollege.net>
2024-12-02 15:24:51 -0500

    Implemented controller for product

--- ECommerce.Api.Products/Startup.cs
+++ ECommerce.Api.Products/Startup.cs
@@ -1,14 +1,19 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+using ECommerce.Api.Products.Providers;
 
 namespace ECommerce.Api.Products
 {
@@ -24,6 +29,14 @@ namespace ECommerce.Api.Products
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductsProvider, ProductsProvider>();
+            services.AddAutoMapper(typeof(Startup));
+           
+            services.AddDbContext<ProductsDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Products");
+            });
+
             services.AddControllers();
         }
 
