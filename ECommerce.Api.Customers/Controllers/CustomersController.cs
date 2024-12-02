d41c4045fb31a8f7c3aed97170c67e3b92341e8c
CompSciZach <2273406@johnabbottcollege.net>
2024-12-02 16:07:57 -0500

    Implemented customer controller. All works. Chapter 2 complete.

--- ECommerce.Api.Customers/Controllers/CustomersController.cs
+++ ECommerce.Api.Customers/Controllers/CustomersController.cs
@@ -1,6 +1,40 @@
-﻿namespace ECommerce.Api.Customers.Controllers
+﻿using ECommerce.Api.Customers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Controllers
 {
-    public class CustomersController
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomersController : ControllerBase
     {
+        private readonly ICustomersProvider customersProvider;
+
+        public CustomersController(ICustomersProvider customersProvider)
+        {
+            this.customersProvider = customersProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomersAsync()
+        {
+            var result = await customersProvider.GetCustomersAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Customers);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomerAsync(int id)
+        {
+            var result = await customersProvider.GetCustomerAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Customer);
+            }
+            return NotFound();
+        }
     }
 }
